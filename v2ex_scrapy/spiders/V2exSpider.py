d3a2dce55c8317d4addd65a05a52b38c85098d3b
oldshensheep <oldshensheep@gmail.com>
2023-07-06 16:23:56 +0800

    fix start_requests

--- v2ex_scrapy/spiders/V2exSpider.py
+++ v2ex_scrapy/spiders/V2exSpider.py
@@ -28,12 +28,13 @@ class V2exTopicSpider(scrapy.Spider):
         self.logger.info(f"start from topic id {self.start_id}, end at {self.end_id}")
 
     def start_requests(self):
+        # 之前的评论和用户信息可能没爬完，所以继续爬停止时的topic
         yield scrapy.Request(
             url=f"https://www.v2ex.com/t/{self.start_id}",
             callback=self.parse,
             cb_kwargs={"topic_id": self.start_id},
         )
-        for i in range(self.start_id, self.end_id + 1):
+        for i in range(self.start_id + 1, self.end_id + 1):
             if self.UPDATE_TOPIC or not self.db.exist(TopicItem, i):
                 yield scrapy.Request(
                     url=f"https://www.v2ex.com/t/{i}",
